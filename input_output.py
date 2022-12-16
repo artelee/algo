3e9d062ccfe5c5ae79a50f9b11c01e1be37b9f0a
artelee <eunhye4611@naver.com>
2022-12-17 08:43:49 +0900

    [bronze] #2501 약수구하기

--- input_output.py
+++ input_output.py
@@ -10,9 +10,14 @@ def get_1000():
 
 
 # 2501번 약수구하기
+# N은 1 이상 10,000 이하이다. K는 1 이상 N 이하이다.
 def get_2501():
     n, k = map(int, input().split())
     n_divisor_list = [(i + 1) for i in range(n) if n % (i + 1) == 0]
-    print(n_divisor_list)
+    # print(n_divisor_list)
+    if len(n_divisor_list) < k:
+        print(0)
+    else:
+        print(n_divisor_list[k-1])
 
 get_2501()
